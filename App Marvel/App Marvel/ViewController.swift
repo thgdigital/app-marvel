558966d59b54e46437b6810a16933d0471dd05b9
Thiago Santos <t.vaz.santos@gmail.com>
2021-12-01 10:33:03 -0300

    Criando projeto e inserindo arquivos

---
+++ App Marvel/App Marvel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App Marvel
+//
+//  Created by Thiago Vaz on 29/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print(Date.now.timeIntervalSince1970 * 1000)
+    }
+
+
+}
+
---
+++ App Marvel/App MarvelTests/App_MarvelTests.swift
@@ -0,0 +1,33 @@
+//
+//  App_MarvelTests.swift
+//  App MarvelTests
+//
+//  Created by Thiago Vaz on 29/11/21.
+//
+
+import XCTest
+@testable import App_Marvel
+
+class App_MarvelTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
